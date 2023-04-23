ef3576ccd68f51c32cc5436ceb9505a1acac5d80
TG8211 <jwad1425@gmail.com>
2023-04-23 13:05:26 +0300

    fixed deployment issues

---
+++ public/_nuxt/static/1682244318/wireframing/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1682244318",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwireframing",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bbde323a"}}}}(null));
